ba44108351bc4f0d212a45f693750aa83dffd55f
Kacper Kuszek <kuszekkacper@gmail.com>
2026-01-07 18:17:11 +0100

    Added shoot input

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,23 +1,36 @@
+using StarterAssets;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-        int i = 1;
+    StarterAssetsInputs starterAssetsInputs;
+
+    private void Awake()
+    {
+       starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
+
+    int i = 1;
 
 
     void Update()
     {
-        RaycastHit hit;
-        if(Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward,out hit,Mathf.Infinity))
-        {
-            Debug.Log(hit.collider.name +  i);
-            i++;
 
-        }else 
+        if (starterAssetsInputs.shoot)
         {
-            Debug.Log($"Its null {i}");
+            RaycastHit hit;
+            if(Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward,out hit,Mathf.Infinity))
+            {
+                Debug.Log(hit.collider.name +  i);
+                i++;
+            }else 
+            {
+                Debug.Log($"Its null {i}");
+            }
+            starterAssetsInputs.ShootInput(false);
         }
-    
+
+
     }
 }
